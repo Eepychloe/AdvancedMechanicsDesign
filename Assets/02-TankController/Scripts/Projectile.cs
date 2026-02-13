03ffaebc24c17c725f0f99ced25645ad2d9b1159
Eepychloe <eepychloe@outlook.com>
2026-02-13 17:18:21 +0000

    Ver smol change

--- Assets/02-TankController/Scripts/Projectile.cs
+++ Assets/02-TankController/Scripts/Projectile.cs
@@ -5,18 +5,22 @@ public class Projectile : MonoBehaviour
 {
     //For testing
     [SerializeField] private GameObject projectilePrefab;
+    [SerializeField] private Rigidbody rb;
+    private float velocity;
+    private int steps;
     private Trajectory trajectory;
 
     void Start()
     {
         trajectory = GetComponent<Trajectory>();
+        rb = GetComponent<Rigidbody>();
     }
 
     public void SpawnProjectile(GameObject firePoint)
     {
         
         Instantiate(projectilePrefab, firePoint.transform.position, firePoint.transform.rotation);
-        vector3 pos = trajectory.Plot(firePoint.transform.position);
+        //Vector3 pos = trajectory.Plot(rb, firePoint.transform.position, velocity, steps);
     }
 
 
